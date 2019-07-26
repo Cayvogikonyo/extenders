5d8b8e79413153ae415a16511ef8613189dd780b
kelvin.gikonyo <kevohm.km@gmail.com>
2019-07-27 00:03:41 +0300

    Extenders website

---
+++ index.php
@@ -0,0 +1,119 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Extenders.org</title>
+
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="assets/css/styles.css" >
+    <link rel="stylesheet" href="assets/css/all.css" >
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-light nav-bottom" style="margin: auto;">
+
+    <a href="#" class="navbar-left pop-logo"><img src="assets/images/logo.png" style="width: 100%;"></a>
+
+
+    <!-- The navigation bar -->
+    <div class="nav-wrapper">
+
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#extendersNav"
+      aria-controls="extendersNav" aria-expanded="false" aria-label="Toggle extenders navigation">
+        <span class="navbar-toggler-icon"></span>
+
+      </button>
+
+      <div class="collapse navbar-collapse" id="extendersNav" >
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#extenders">Extenders</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#testimonials">Our Journey</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#gallery">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>
+        </ul>
+      <!--  <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>-->
+      </div>
+
+    </div>
+
+  </nav>
+
+
+    <!--definition of body contents -->
+
+    <!-- Home carousel. To be upgraded to pick images dynamically from database-->
+  <!--  <div class="carousel slide header-images" data-ride="carousel" id="homeCarousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+                <img class="d-block w-100" src="images/image1.jpg?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+        </div>
+        <div class="carousel-item">
+                <img class="d-block w-100" src="images/image2.jpg?auto=yes&bg=777&fg=555&text=Second slide" alt="Second slide">
+        </div>
+
+        <div class="carousel-item">
+                <img class="d-block w-100" src="images/image3.jpg?auto=yes&bg=777&fg=555&text=Third slide" alt="Third slide">
+        </div>
+
+        <div class="carousel-item">
+                <img class="d-block w-100" src="images/image4.jpg?auto=yes&bg=777&fg=555&text=Fourth slide" alt="Fourth slide">
+        </div>
+
+      </div>
+
+    </div>-->
+
+    <div class="header-images">
+
+    </div>
+
+    <section class="us">
+      <div class="container profile" id="extenders">
+        <h1>Imagine a happy world,<br> <span>a smiley World.</span></h1>
+        <p>We do what we do because we believe in our hearts everyone deserves a smile. Extenders is a joint hands movement of
+          friends and any willing participant set up to try put a smile on the face of the less fortunate, street families, the
+          sick and the needy.Take a tour..</p>
+
+        <h4>What we do</h4>
+
+        <p>We are commited to expand the boundaries of happiness and we do everything in our capabilities to do so. We visit orphanage homes and street homes trying to put a smile on that unfortunate soul, sometimes lonely. And while at it, our smile widens too.</p>
+
+        <h4>Our Passion</h4>
+
+      <p>The idea of everyone being at least a little bit happier fuels our ambitions pushing us to try and reach more beautiful hearts. We are joyous to give a children's home a weekend worth of memories, memories of joy, fun and probably lots of dancing. Who doesn't fancy dancing. To leave a place and feel a strong invite for a probable next time keeps us going. </p>
+      </div>
+      <a href="#" class="scroll">  <i class="fas fa-angle-up"></i> <!-- uses light style --></a>
+    </section>
+
+    <section id="testimonials" class="testimonials">
+
+    </section>
+
+    <section id="gallery" class="gallery">
+
+    </section>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js"
+    integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+    <footer class="contact" id="contact">
+
+    </footer>
+  </body>
+</html>
